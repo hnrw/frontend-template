b5c19dc62715a4f51e740c5f25596a650c760d01
Henry <henry@henrywu.co.uk>
2020-11-05 21:21:42 +0000

    create template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "@material-ui/core"
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from "react-router-dom"
 
-function App() {
+import Component from "./components/Component"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Router>
+        <Switch>
+          <Route path="/test">
+
+          </Route>
+          <Route path="/">
+            <Component />
+          </Route>
+        </Switch>
+      </Router>
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/reducers/testReducer.js
@@ -0,0 +1,19 @@
+export const addAnswer = (answer) => {
+  return {
+    type: "ADD_ANSWER",
+    answer
+  }
+}
+
+const answerReducer = (state = [], action) => {
+  switch(action.type) {
+    case "ADD_ANSWER":
+      return state.concat(action.answer)
+    case "RESET_ANSWERS":
+      return []
+    default:
+      return state
+  }
+}
+
+export default answerReducer
